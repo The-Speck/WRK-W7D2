fd38962947519aa1d009f2438ca7124a0ecb4bcc
Alex <a.plyushchay@gmail.com>
2018-07-03 14:24:48 -0400

    Phase 2: Putting it all together

---
+++ todos/app/controllers/api/todos_controller.rb
@@ -0,0 +1,39 @@
+class Api::TodosController < ApplicationController
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def index
+    render json: Todo.all
+  end
+
+  def create
+    @todo = Todo.new(todos_params)
+
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todos_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    index
+  end
+
+  def todos_params
+    params.require(:todo).permit(:title, :body)
+  end
+end
--- todos/webpack.config.js
+++ todos/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/todo_redux.jsx',
+  entry: './app/assets/javascripts',
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
